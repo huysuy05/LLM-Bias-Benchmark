7bcf407977de278c1e0c0e12ef09927ecf381ef2
huysuy05 <huynguyen23@augustana.edu>
2025-10-08 19:48:26 -0500

    Updated shell script for running mlx models evals

--- shell_scripts/run_evals_with_mlx.sh
+++ shell_scripts/run_evals_with_mlx.sh
@@ -6,6 +6,7 @@ SHOT_MAJ=8
 TOP_P=0
 TEMPERATURE=0
 MAX_TOKENS=10
+MAJORITY_LABEL="sports" #This label is in agnews
 
 # Run Python script
 python3 src/eval_mlx_models.py --model $MODEL \
@@ -13,4 +14,5 @@ python3 src/eval_mlx_models.py --model $MODEL \
                             --shot-minority $SHOT_MIN --shot-majority $SHOT_MAJ \
                             --temperature $TEMPERATURE \
                             --top-p $TOP_P \
-                            --max-tokens $MAX_TOKENS
+                            --max-tokens $MAX_TOKENS \
+                            --majority-label $MAJORITY_LABEL
