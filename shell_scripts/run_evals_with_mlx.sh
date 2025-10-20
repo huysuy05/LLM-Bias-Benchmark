8a3fd94563942ebf245b28137a3af4ad5bae6e1f
huysuy05 <huynguyen23@augustana.edu>
2025-10-20 18:05:27 -0500

    Tested with AG_news for Gemma 1B for both ICL and SC

--- shell_scripts/run_evals_with_mlx.sh
+++ shell_scripts/run_evals_with_mlx.sh
@@ -1,4 +1,4 @@
-MODEL="openai-community/gpt2"
+MODEL="google/gemma-3-1b-it"
 # More models to test: 
 # Qwen/Qwen2.5-0.5B-Instruct
 # google/gemma-3-270m
@@ -11,22 +11,22 @@ SHOT_MIN=8
 SHOT_MAJ=8
 TOP_P=0
 TEMPERATURE=0
-MAX_TOKENS=10
+MAX_TOKENS=256
 MAJORITY_LABEL="sports" #This label is in agnews
 
 # Run Python script
-python3 src/eval_mlx_models.py --model $MODEL \
-                            --datasets $DATASETS \
-                            --shot-minority $SHOT_MIN --shot-majority $SHOT_MAJ \
-                            --temperature $TEMPERATURE \
-                            --top-p $TOP_P \
-                            --max-tokens $MAX_TOKENS \
-                            --majority-label $MAJORITY_LABEL
+# python3 src/eval_mlx_models.py --model $MODEL \
+#                             --datasets $DATASETS \
+#                             --shot-minority $SHOT_MIN --shot-majority $SHOT_MAJ \
+#                             --temperature $TEMPERATURE \
+#                             --top-p $TOP_P \
+#                             --max-tokens $MAX_TOKENS \
+#                             --majority-label $MAJORITY_LABEL
 
 # (Optional) Script for running with self_consistency
-# python3 src/eval_mlx_models.py \
-#     --model $MODEL \
-#     --datasets $DATASETS \
-#     --use-self-consistency \
-#     --sc-samples 1 \
-#     --sc-temperature 0.6
+python3 src/eval_mlx_models.py \
+    --model $MODEL \
+    --datasets $DATASETS \
+    --use-self-consistency \
+    --sc-samples 3 \
+    --sc-temperature 0.6
