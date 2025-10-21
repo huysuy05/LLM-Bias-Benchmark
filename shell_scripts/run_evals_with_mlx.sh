63bbff7c568d5b265bfdc778476a98d394c77017
huysuy05 <huynguyen23@augustana.edu>
2025-10-21 00:36:50 -0500

    tested with gemma 1b for ICL

--- shell_scripts/run_evals_with_mlx.sh
+++ shell_scripts/run_evals_with_mlx.sh
@@ -11,22 +11,22 @@ SHOT_MIN=4
 SHOT_MAJ=4
 TOP_P=0
 TEMPERATURE=0
-MAX_TOKENS=256
+MAX_TOKENS=16
 MAJORITY_LABEL="sports" #This label is in agnews
 
 # Run Python script
-# python3 src/eval_mlx_models.py --model $MODEL \
-#                             --datasets $DATASETS \
-#                             --shot-minority $SHOT_MIN --shot-majority $SHOT_MAJ \
-#                             --temperature $TEMPERATURE \
-#                             --top-p $TOP_P \
-#                             --max-tokens $MAX_TOKENS \
-#                             --majority-label $MAJORITY_LABEL
+python3 src/eval_mlx_models.py --model $MODEL \
+                            --datasets $DATASETS \
+                            --shot-minority $SHOT_MIN --shot-majority $SHOT_MAJ \
+                            --temperature $TEMPERATURE \
+                            --top-p $TOP_P \
+                            --max-tokens $MAX_TOKENS \
+                            --majority-label $MAJORITY_LABEL
 
 # (Optional) Script for running with self_consistency
-python3 src/eval_mlx_models.py \
-    --model $MODEL \
-    --datasets $DATASETS \
-    --use-self-consistency \
-    --sc-samples 3 \
-    --sc-temperature 0.6
+# python3 src/eval_mlx_models.py \
+#     --model $MODEL \
+#     --datasets $DATASETS \
+#     --use-self-consistency \
+#     --sc-samples 3 \
+#     --sc-temperature 0.6
